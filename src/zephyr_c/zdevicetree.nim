c927ee5415985b56e15969a8d3c7396bc0671fe2
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-09-24 21:35:17 -0700

    setting up pass through macro

--- src/zephyr_c/zdevicetree.nim
+++ src/zephyr_c/zdevicetree.nim
@@ -4,7 +4,10 @@ export cmtoken
 
 # template DT_NODELABEL*(label: cminvtoken): cminvtoken = CTOKEN(label)
 
-CM_DECLARE_PROC(DT_NODELABEL, label)
+# CM_DECLARE_PROC(DT_NODELABEL)
+
+template DT_NODELABEL*(ma: untyped): cminvtoken =
+  CM_PROC(DT_NODELABEL, ma)
 
 # proc DT_NODELABEL*(node_id: cminvtoken): cminvtoken {.importc: "DT_NODELABEL", header: "devicetree.h".}
 
