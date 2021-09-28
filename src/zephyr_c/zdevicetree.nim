96e954835dc41de573fbbad0ab067137f74d99cd
Jaremy J. Creechley <jaremy.creechley@wavebaselabs.com>
2021-09-28 14:11:02 -0700

    adding useful device tree wrappers

--- src/zephyr_c/zdevicetree.nim
+++ src/zephyr_c/zdevicetree.nim
@@ -5,4 +5,9 @@ export cmtoken
 template DT_NODELABEL*(ma: untyped): cminvtoken =
   CM_PROC(DT_NODELABEL, ma)
 
+template DT_ALIAS*(ma: untyped): cminvtoken =
+  CM_PROC(DT_ALIAS, ma)
+
+proc DT_LABEL*(name: cminvtoken): cstring {.importc: "$1", header: "device.h".}
+
 
