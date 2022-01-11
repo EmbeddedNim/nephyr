b15dd38839c767f913ce4046f99cef1f36c392fd
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-01-10 18:34:51 -0800

    adding delay(s)

--- src/nephyr/times.nim
+++ src/nephyr/times.nim
@@ -1,5 +1,7 @@
 
 import mcu_utils/basictypes
+import zephyr_c/kernel
+
 from std/monotimes import MonoTime
 
 export basictypes
@@ -23,3 +25,23 @@ when defined(CONFIG_TIMER_HAS_64BIT_CYCLE_COUNTER):
   proc micros*(): Micros = Micros(k_cyc_to_us_floor64(k_cycle_get_64()))
 else:
   proc micros*(): Micros = Micros(k_cyc_to_us_floor64(k_cycle_get_32()))
+
+proc delay*(ms: Millis): Millis {.discardable.} =
+  ## Sleep for millis, return 0 if requested time elapsed, otherwise the number of millis remaining
+  return k_msleep(ms.int32).Millis
+
+proc delay*(us: Micros): Micros {.discardable.} =
+  ## Sleep for micros, return 0 if requested time elapsed, otherwise the number of micros remaining
+  return k_usleep(us.int32).Micros
+
+proc delayMillis*(ts: int): bool =
+  ## Sleep for millis, return false if woken up early
+  let res = k_msleep(ts.int32)
+  if res == 0:
+    return true
+
+proc delayMicros*(ts: int): bool =
+  ## Sleep for micros, return false if woken up early
+  let res = k_msleep(ts.int32)
+  if res == 0:
+    return true
