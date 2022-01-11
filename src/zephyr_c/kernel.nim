226ca1cf773febc61f1166a464441ff389dd4106
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-01-10 18:24:39 -0800

    adding us and ms sleep

--- src/zephyr_c/kernel.nim
+++ src/zephyr_c/kernel.nim
@@ -48,3 +48,54 @@ proc k_thread_create*(new_thread: ptr k_thread,
                      options: uint32,
                      delay: k_timeout_t): k_tid_t {.
                         importc: "k_thread_create", header: "<kernel.h>".}
+
+proc k_sleep*(timeout: k_timeout_t): int32 {.importc: "k_sleep", header: "<kernel.h>"} ##\
+  ##  * @brief Put the current thread to sleep.
+  ##  *
+  ##  * This routine puts the current thread to sleep for @a duration,
+  ##  * @note if @a timeout is set to K_FOREVER then the thread is suspended.
+  ##  *
+  ##  * @param timeout Desired duration of sleep.
+  ##  *
+  ##  * @return Zero if the requested time has elapsed or the number of milliseconds
+  ##  * left to sleep, if thread was woken up by \ref k_wakeup call.
+
+proc k_msleep*(ms: int32): int32 {.importc: "k_msleep", header: "<kernel.h>"} ##\
+  ##  * @brief Put the current thread to sleep.
+  ##  *
+  ##  * This routine puts the current thread to sleep for @a duration milliseconds.
+  ##  *
+  ##  * @param ms Number of milliseconds to sleep.
+  ##  *
+  ##  * @return Zero if the requested time has elapsed or the number of milliseconds
+  ##  * left to sleep, if thread was woken up by \ref k_wakeup call.
+  ##  */
+
+proc k_usleep*(us: int32): int32 {.importc: "k_usleep", header: "<kernel.h>"} ##\
+  ## /**
+  ##  * @brief Put the current thread to sleep with microsecond resolution.
+  ##  *
+  ##  * This function is unlikely to work as expected without kernel tuning.
+  ##  * In particular, because the lower bound on the duration of a sleep is
+  ##  * the duration of a tick, @kconfig{CONFIG_SYS_CLOCK_TICKS_PER_SEC} must be
+  ##  * adjusted to achieve the resolution desired. The implications of doing
+  ##  * this must be understood before attempting to use k_usleep(). Use with
+  ##  * caution.
+  ##  *
+  ##  * @param us Number of microseconds to sleep.
+  ##  *
+  ##  * @return Zero if the requested time has elapsed or the number of microseconds
+  ##  * left to sleep, if thread was woken up by \ref k_wakeup call.
+  ##  */
+
+proc k_yield*() {.importc: "k_yield", header: "<kernel.h>"} ##\
+  ## /**
+  ##  * @brief Yield the current thread.
+  ##  *
+  ##  * This routine causes the current thread to yield execution to another
+  ##  * thread of the same or higher priority. If there are no other ready threads
+  ##  * of the same or higher priority, the routine returns immediately.
+  ##  *
+  ##  * @return N/A
+  ##  */
+
