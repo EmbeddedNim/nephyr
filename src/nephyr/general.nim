86c3a66fe168a5c2b2a5a8d73335ac68532257cf
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-02-01 15:43:25 -0800

    add usb enable api

--- src/nephyr/general.nim
+++ src/nephyr/general.nim
@@ -18,3 +18,6 @@ export logs
 proc sysReboot*(coldReboot: bool = false) = k_sys_reboot(if coldReboot: 1 else: 0)
 proc sysPanic*(reason: k_fatal_error_reason | cuint) = k_fatal_halt(reason.cuint)
 proc sysPanic*() = k_fatal_halt(K_ERR_KERNEL_PANIC.cuint)
+
+when CONFIG_USB_DEVICE_STACK:
+  proc usbEnable*(arg: pointer = nil): cint {.importc: "usb_enable", header: "<usb/usb_device.h>", discardable.}
