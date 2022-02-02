0dae8b1bb62c151d0c87e82203560dca4bad7527
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-02-02 03:24:24 +0000

    add initial delay after main for debugging

--- src/nephyr.nim
+++ src/nephyr.nim
@@ -11,8 +11,13 @@ include mcu_utils/threads
 proc NimMain() {.importc.}
 
 template app_main*(blk: untyped): untyped =
+  app_main(0, blk)
+
+template app_main*(delayms: static[int], blk: untyped): untyped =
 
   proc main*() {.exportc.} =
+    when delayms > 0:
+      os.sleep(delayms)
     NimMain() # initialize garbage collector memory, types and stack
     try:
       blk
