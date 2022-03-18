feb4dbec840b85cb85a924f633d4092562fe2cad
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-03-17 18:10:37 -0700

    adding in some basic iface interfaces

---
+++ src/nephyr/nets.nim
@@ -0,0 +1,74 @@
+import std/[net, os, tables]
+
+import mcu_utils/logging
+
+import nephyr
+
+import zephyr_c/net/znet_linkaddr
+import zephyr_c/net/znet_ip
+import zephyr_c/net/znet_if
+import zephyr_c/net/znet_config
+import zephyr_c/net/zipv6
+
+export znet_linkaddr, znet_ip, znet_if, znet_config, zipv6
+
+import std/posix 
+import std/nativesockets 
+
+export posix, nativesockets, net
+
+type
+  NetIfId* = distinct int
+
+  NetIfDevice* = object
+    iface*: ptr net_if
+
+
+# void net_if_foreach(net_if_cb_t cb, void *user_data)
+proc zfind_interfaces(iface: ptr net_if; user_data: pointer) =
+  var table = cast[TableRef[NetIfId, NetIfDevice]](user_data)
+  let nid = NetIfId(table.len())
+  let nif = NetIfDevice(iface: iface)
+  table[nid] = nif
+
+proc findAllInterfaces*(): TableRef[NetIfId, NetIfDevice] = 
+  ## Finds all current network interfaces
+  let cb: net_if_cb_t = zfind_interfaces
+  result = newTable[NetIfId, NetIfDevice]()
+  net_if_foreach(cb, result.addr)
+
+proc hasDefaultInterface*(): bool =
+  ## check for default interface
+  let iface: ptr net_if = net_if_get_default()
+  result = not iface.isNil
+
+proc getDefaultInterface*(): NetIfDevice {.raises: [ValueError].} =
+  ## get default interface
+  let iface: ptr net_if = net_if_get_default()
+  if iface.isNil:
+    raise newException(ValueError, "no default interface")
+  result = NetIfDevice(iface: iface)
+
+proc hwAddress*(ifdev: NetIfDevice): seq[uint8] =
+  let ll_addr: net_linkaddr = ifdev.iface.if_dev.link_addr
+  result = newSeq[uint8](ll_addr.len.int)
+  copyMem(result[0].addr, ll_addr.caddr, ll_addr.len.int)
+
+proc hwMacAddress*(ifdev: NetIfDevice): array[6, uint8] =
+  let hwaddr = ifdev.hwAddress()
+  result[0..5] = hwaddr[0..5]
+
+proc linkLocalAddr*(ifdev: NetIfDevice): IpAddress {.raises: [ValueError].} =
+  ## finds and returns link local address
+  let lladdr: ptr In6Addr = net_if_ipv6_get_ll(ifdev.iface, NET_ADDR_ANY_STATE)
+
+  if lladdr.isNil:
+    raise newException(ValueError, "no ipv6 link-local addr")
+
+  var
+    saddr: Sockaddr_in6
+    port: Port
+  
+  saddr.sin6_family = toInt(Domain.AF_INET6).TSa_Family
+  saddr.sin6_addr = lladdr[]
+  fromSockAddr(saddr, sizeof(saddr).SockLen, result, port)
