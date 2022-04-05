a4b871486490a891075f280e5d29c0274d43d551
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-04-05 04:46:47 +0000

    simplify

--- src/nephyr/board_fixes.nim
+++ src/nephyr/board_fixes.nim
@@ -2,14 +2,11 @@ import zconsts
 
 when BOARD in ["teensy40", "teensy41"]:
   ## Change Teensy PinMux to use CS GPIO Pin
-  # type MuxCfg* = distinct int
-  # var IOMUXC_GPIO_AD_B0_03_GPIO1_IO03* {.importc: "$1", header: "<fsl_iomuxc.h>".}: MuxCfg
-  # proc IOMUXC_SetPinMux*(muxCfg: MuxCfg, val: cint) {.importc: "IOMUXC_SetPinMux", header: "<fsl_iomuxc.h>".}
 
   proc board_configuration() {.exportc.} =
     {.emit: """
     #include <fsl_iomuxc.h>
-    IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B0_03_GPIO1_IO03, 0)
+    IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B0_03_GPIO1_IO03, 0);
     """.}
 
   SystemInit(board_configuration, INIT_PRE_KERNEL_1, 40)
