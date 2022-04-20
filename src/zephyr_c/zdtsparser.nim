3b1d654913ae4efcc1ecca84152f09d96f170794
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-04-20 05:13:13 +0000

    refactor

--- src/zephyr_c/zdtsparser.nim
+++ src/zephyr_c/zdtsparser.nim
@@ -42,31 +42,21 @@ let parser = peg("props", state: ParserState):
   customTarget <- "add_custom_target(" * +word * ")"
 
   targetProps <- "set_target_properties(devicetree_target PROPERTIES " * >dtProps * ")":
-    # echo "targetProps: ", $1
     discard
 
-  allLessParen <- 1 - ' '
-  ps <- '"' * +path * '"'
   dtParams <- dtParams3 | dtParams2 | E"params error"
   dtParams2 <- '"' * >dtKind * '|' * >+path * '"':
-    # echo "dtKind2: parent: ", $1, " path: ", $2
     state.key = $2
     state.curr = DtAttrs(name: $2, kind: parseEnum[DtKind]($1))
-    # state.props.mgetOrPut($2, newTable[string, string]())[$1] = $2
-    discard
   dtParams3 <- '"' * >dtKind * '|' * >+path * '|' * >+path * '"':
-    # echo "dtKind3: kind: ", $1, " path: ", $2, " label: ", $3
     state.key = $2
     state.curr = DtAttrs(name: $3, kind: parseEnum[DtKind]($1))
-    # state.props.mgetOrPut($2, newTable[string, string]())[$1] = $3
-    discard
 
   dtProps <- dtNode | dtProperty | E"dt prop error"
   dtNode <- dtParams * +Space * "TRUE"
   dtProperty <- dtParams * +Space * (>"\"\"" | '"' * >+path * '"'):
     let curr = move state.curr
     curr.value = $1
-    # echo "DT_PROP: ", state.key, " curr: ", curr
     state.nodes.mgetOrPut(state.key, newSeq[DtAttrs]()).add curr
 
   dtKind <- ("DT_NODELABEL" | "DT_NODE" | "DT_PROP" | "DT_REG" | "DT_CHOSEN" | E"unsupported dt tag")
