97f4a254a6c10728736c47447a8745f43a67f34a
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-06-08 09:40:00 +0000

    print listall static devices

--- src/nephyr/drivers/device.nim
+++ src/nephyr/drivers/device.nim
@@ -1,9 +1,20 @@
 import ../zephyr/zdevice
 
+proc z_device_get_all_static*(device: ptr UncheckedArray[ptr device]): csize_t {.
+    importc: "z_device_get_all_static", header: "device.h".} ##\
+      ## @brief Get access to the static array of static devices.
+      ## 
+      ## @param devices where to store the pointer to the array of
+      ## statically allocated devices. The array must not be mutated
+      ## through this pointer.
+      ## *
+      ## @return the number of statically allocated devices.
+      ## 
 
 proc listAllStaticDevices*(): seq[ptr device] =
-  var sdevs: ptr UncheckedArray[device]
-  let cnt = z_device_get_all_static(cast[ptr ptr device](addr sdevs))
+  var sdevs: ptr UncheckedArray[ptr device]
+  var cnt: csize_t
+  cnt = z_device_get_all_static(sdevs)
   newSeq(result, cnt)
   for i in 0..<cnt:
-      result[i] = addr(sdevs[i])
+      result[i] = sdevs[i]
